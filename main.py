64e3805b0c4d151f2b10fa666f8e083d0366b77c
yujale98 <yujale98@gmail.com>
2023-06-06 14:08:12 +0800

    脚本编写

---
+++ main.py
@@ -0,0 +1,10 @@
+# 这是一个示例 Python 脚本。
+
+# 按 ⌃R 执行或将其替换为您的代码。
+# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+from selenium import webdriver
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    browser = webdriver.Chrome()
